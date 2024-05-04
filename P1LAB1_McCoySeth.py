906313ab14131e3b2370f3ff965a23e0262f8463
smccoy04 <163803417+smccoy04@users.noreply.github.com>
2024-05-04 11:48:02 -0400

    Add files via upload

---
+++ P1LAB1_McCoySeth.py
@@ -0,0 +1,11 @@
+# Seth McCoy
+# 5/4/2024
+# P1LAB1
+# This assignment teaches the student how to create a Python program using IDLE. It shows how to get input from the user and display output.
+
+# Prompt user to enter their first name
+first_name = input("Enter your first name: ")
+
+# Print welcome message using the entered first name
+print("Hey", first_name)
+print("Welcome to zybooks!")
